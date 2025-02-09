c4131b9336a5c6a6025cc83947da47a112e8782c
Salvatore Amaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2025-02-09 13:32:49 +0000

    Add project files.

---
+++ InternetListener.cs
@@ -0,0 +1,70 @@
+﻿using System.Runtime.InteropServices;
+
+namespace WinInternetListener
+{
+    public class InternetListener
+    {
+        [DllImport("wininet.dll")]
+        private static extern bool InternetGetConnectedState(out int description, int reservedValue);
+        private static readonly Lazy<InternetListener> _instance = new(() => new());
+        public static InternetListener Value => _instance.Value;
+
+        private volatile bool _isConnected = true;
+        public bool IsConnected => _isConnected;
+        public int CheckEvery { get; set; } = 2000;
+        public event EventHandler? ConnectionLost;
+        public event EventHandler? ConnectionGained;
+        private CancellationTokenSource? _cts;
+
+        private InternetListener()
+        {
+
+        }
+
+        public void Listen()
+        {
+            if (Interlocked.CompareExchange(ref _cts, new CancellationTokenSource(), null) != null)
+                return;
+
+            _cts = new CancellationTokenSource();
+            Task.Run(() => ListenAsync(_cts.Token), _cts.Token);
+        }
+
+        public void Stop()
+        {
+            _cts?.Cancel();
+            _cts = null;
+        }
+
+        private async Task ListenAsync(CancellationToken token)
+        {
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    bool temp = InternetGetConnectedState(out _, 0);
+
+                    if (temp != IsConnected)
+                    {
+                        _isConnected = temp;
+
+                        if (_isConnected)
+                            ConnectionGained?.Invoke(this, EventArgs.Empty);
+                        else
+                            ConnectionLost?.Invoke(this, EventArgs.Empty);
+                    }
+
+                    await Task.Delay(CheckEvery, token);
+                }
+            }
+            catch (TaskCanceledException)
+            {
+                // Task was canceled gracefully
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error in ListenAsync: {ex.Message}");
+            }
+        }
+    }
+}
